20e5659a857f51e62e0789ae307d7de736fab959
Fachrin Aulia <fachrin.nasution@deliveryhero.com>
2022-06-25 10:57:26 +0200

    add non decreasing array solution

---
+++ array/non-decreasing-array.py
@@ -0,0 +1,32 @@
+# Leetcode: https://leetcode.com/problems/non-decreasing-array/
+
+def checkPossibility(nums):
+    l, r = 0, 1
+    counter, nums_len = 0, len(nums)
+    
+    while r < len(nums) and counter <= 1:
+        if nums[l] <= nums[r]:
+            l += 1
+            r += 1
+            continue
+            
+        if l == 0 or r == nums_len - 1:
+            pass
+        elif l > 0 and nums[l - 1] <= nums[r]:
+            nums[l] = nums[l - 1]
+        elif r + 1 < nums_len and nums[l] <= nums[r + 1]:
+            nums[r] = nums[l]
+        else:
+            counter = 100 # considered as max number
+        
+        l += 1
+        r += 1
+        counter += 1
+        
+    return counter <= 1
+
+
+if __name__ == '__main__':
+    assert checkPossibility([4,2,3]) == True
+    assert checkPossibility([4,2,1]) == False
+    assert checkPossibility([1, 5, 4, 6]) == True
