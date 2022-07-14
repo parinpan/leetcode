de0dedddd0dd474bea19f25df6a05d6d77168511
Fachrin Aulia <fachrin.nasution@deliveryhero.com>
2022-07-14 15:19:30 +0200

    renew lru impl

--- cache/lru.py
+++ cache/lru.py
@@ -9,79 +9,60 @@ class LinkedList:
 
 
 class LRUCache:
-    def __init__(self, capacity: int):
-        self.tail = None
-        self.list = None
-        self.cache = {}
+    def __init__(self, capacity):
+        self.node = {}
         self.capacity = capacity
 
-    def remove_node(self, key):
-        if key not in self.cache:
-            return
-        
-        # get node from cache
-        node = self.cache.pop(key)
+        self.head = LinkedList(None, None)
+        self.tail = LinkedList(None, None)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
-        # handle a single node in the linked-list
-        if node.prev == None and node.next == None:
-            return
-
-        # handle a head node in the linked-list
-        if node.prev == None and node.next != None:
-            self.list = self.list.next
-            return
-
-        # handle last node in the linked-list
-        if node.next == None:
-            self.tail = node.prev
-
-            if self.tail != None:
-                self.tail.next = None
-        else:
-            node.prev.next = node.next
-            node.next.prev = node.prev
-
-        return
-
-    def get(self, key: int) -> int:
-        node = self.cache.get(key, None)
-
-        if node == None:
+    def get(self, key):
+        if key not in self.node:
             return -1
 
-        value = node.value
-        self.put(node.key, value)
+        value = self.node[key].value
+        self.put(key, value)
 
         return value
 
-    def put(self, key: int, value: int) -> None:
-        if self.capacity == 0:
+    def put(self, key, value):
+        if key in self.node:
+            self.remove(key)
+            self.put(key, value)
             return
-
-        if self.capacity == 1:
-            self.cache = {key: LinkedList(key, value)}
-            return
-
-        l = LinkedList(key, value)
-        self.remove_node(key)
-
-        if self.list == None:
-            self.list = l
-            self.tail = l
-            self.cache[key] = l
-            return
-
-        if len(self.cache) + 1 > self.capacity:
-            self.cache.pop(self.tail.key)
-            self.tail = self.tail.prev
-            
-            if self.tail != None:
-                self.tail.next = None
         
-        # refer cache to the node
-        self.cache[key] = l
+        node = LinkedList(key, value)
+        node.value = value
+        temp_next = self.head.next
+        
+        self.head.next = node
+        node.next = temp_next
+        node.prev = self.head
+        temp_next.prev = node
 
-        # move latest node to head
-        l.next = self.list
-        self.list.prev = l
-        self.list = l
+        if len(self.node) + 1 > self.capacity:
+            self.remove(self.tail.prev.key)
+
+        self.node[key] = node
+
+    def remove(self, key):
+        node = self.node[key]
+        self.node.pop(key)
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+
+if __name__ == '__main__':
+    cache = LRUCache(4)
+    cache.put(1, 1)
+    cache.put(2, 2)
+    cache.put(3, 3)
+    cache.put(4, 4)
+    cache.put(5, 5)
+    
+    assert cache.get(1) == -1
+    assert cache.get(2) == 2
+    assert cache.get(4) == 4
+    assert [cache.head.next.key, cache.head.next.next.key, cache.head.next.next.next.key, cache.head.next.next.next.next.key] == [4, 2, 5, 3]
